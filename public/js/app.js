edb12e2d4ee99a8d579342d6fa13ded8b597c636
Reddy <reddynarasimha846@gmail.com>
2019-09-08 14:26:39 +0530

    initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side JS file loaded')
+//console.log('Client side JS file loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
