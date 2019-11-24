dd10846948d0ddb673d595b22dde9104289edbb9
teja <tejareddy.teju@gmail.com>
2019-11-25 01:32:02 +0530

    Added location button in the front-end

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 //console.log('Client side JS file loaded')
 
 const weatherForm = document.querySelector('form')
+const getlocation = document.querySelector('#get-location')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
 const messageTwo = document.querySelector('#message-two')
@@ -25,5 +26,25 @@ weatherForm.addEventListener('submit', (event) => {
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecastData
         })
+    })
+})
+
+getlocation.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    messageOne.textContent = 'Loading!!!!'
+    messageTwo.textContent = ''
+
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser!')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        let position = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+
+        // Make REST call to new API for forecat information.
     })
 })
